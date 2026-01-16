58f4bdde63c1975934e85d03ee39f2ebd073327b
Danijel Simeunovic <danijel.simeunovic@trumf.no>
2026-01-16 15:00:32 +0100

    config undo

--- src/config/spotify.ts
+++ src/config/spotify.ts
@@ -1,10 +1,10 @@
-import { useRuntimeConfig } from '../hooks/useRuntimeConfig';
+import { getRuntimeConfig } from '../hooks/useRuntimeConfig';
 
 // Get runtime config (falls back to compile-time env vars in development)
-const { config } = useRuntimeConfig();
+const runtimeConfig = getRuntimeConfig();
 
-export const CLIENT_ID = config?.VITE_SPOTIFY_CLIENT_ID || '011c5f27eef64dd0b6f65ca673215a58';
+export const CLIENT_ID = runtimeConfig.VITE_SPOTIFY_CLIENT_ID;
 export const REDIRECT_URI = window.location.origin;
 export const SCOPES = 'streaming user-read-email user-read-private user-modify-playback-state';
-export const API_URL = config?.VITE_API_URL || 'http://localhost:4001';
+export const API_URL = runtimeConfig.VITE_API_URL;
 
