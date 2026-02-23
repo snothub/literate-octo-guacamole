9a8ced56641082da1c2c2a393be366c4c387ee67
Danijel Simeunovic <danijel.simeunovic@trumf.no>
2026-02-23 13:21:04 +0100

    dev changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'nonapprehensible-mesenteric-catheryn.ngrok-free.dev',
+      'a91a-213-236-251-22.ngrok-free.app',
       'a68b983276d5.ngrok-free.app'
     ],
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4000',
+        changeOrigin: true,
+      },
+    },
   },
 })
